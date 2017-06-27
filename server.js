25893805daf7918db325cbb3c6a68487629a06f5
Josh <josue0991@icloud.com>
2017-06-26 19:10:20 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const accepts = require("accepts");
+const useragent = require("express-useragent");
+
+app.use(useragent.express());
+
+app.get("/", function(req, res){
+	const user = {
+	software: req.useragent.source.match(/\((.+?)\)/)[1],
+	language: accepts(req).language()[0],
+	ipaddress: req.ip,	
+	};
+	
+	res.end(JSON.stringify(user));
+});
+app.listen(process.env.PORT || 3000);

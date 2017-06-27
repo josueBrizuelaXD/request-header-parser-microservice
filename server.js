a339fb0de7258bec3e89413456fe0d8764c56c39
Josh <josue0991@icloud.com>
2017-06-26 19:29:20 -0500

    first commit

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const app = express();
 const accepts = require("accepts");
 const useragent = require("express-useragent");
 
+app.enable("trust proxy");
 app.use(useragent.express());
 
 app.get("/", function(req, res){
 	const user = {
 	software: req.useragent.source.match(/\((.+?)\)/)[1],
 	language: accepts(req).language()[0],
-	ipaddress: req.ip,	
+	ipaddress: req.ip || req.ips,	
 	};
 	
 	res.end(JSON.stringify(user));
